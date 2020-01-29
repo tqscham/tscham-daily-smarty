1ee605daac0bdc5e6b16e78d023635a9993e908e
tqscham <tqscham@outlook.com>
2020-01-28 23:37:23 -0600

    Logo setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+import Logo from "./logo";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>Logo</div>
+        <div className="container">
+          <Logo />
+
+          <div className="search">
+
+          </div>
+
+          <div className="recent">
+
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img alt="daily smarty ui image" src="/assets/ds_circle_logo.png"/>
+            </div>
+        );
+    }
+}

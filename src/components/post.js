f8f1bb7be4907afdda98d90a439d4826a0883c4f
tqscham <tqscham@outlook.com>
2020-02-01 14:07:45 -0600

    added react animate height and implimented into results titles

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 class Post extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            height: 0
+        }
+    }
 
     renderLinks() {
         let links = this.props.post_links.map((link, index) => {
@@ -42,18 +50,25 @@ class Post extends Component {
         } else if (this.props.type === 'result'){
             return (
                 <li className='result-post'>
-                <div className='result-post-title'>
-                        {this.props.title}
-                    </div>
-
                     <div className='result-post-topics'>
                         {this.renderTopics()}
-
                     </div>
 
+                    <div className='result-post-title'>
+                        <a href={this.props.url_for_post}
+                            onMouseEnter={() => this.setState({ height: 70 })}
+                            onMouseLeave={() => this.setState({ height: 0 })} 
+                        >{this.props.title}</a>
+                    </div>
+
+                    <AnimateHeight
+                        duration={500}
+                        height={this.state.height}
+                    >
                     <div className='result-post-links'>
                         {this.renderLinks()}
                     </div>
+                    </AnimateHeight>
                 </li>
             )
         }
